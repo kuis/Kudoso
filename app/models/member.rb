374d102d4cbd2ac546df923bdb6a9bc92847f000
Mike Kazmier <kaz@kaztopia.com>
2014-11-25 18:14:41 -0700

    working highcharts on dashboard

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,14 +12,18 @@ class Member < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
-  def todos(date = Date.today)
-    todos = self.my_todos.where('due_date = ?', date).map.to_a
-    self.todo_schedules.where('start_date <= ? AND (end_date IS NULL OR end_date <= ?)', date, date).find_each do |ts|
-      todo = todos.find{ |td| td.todo_schedule_id == ts.id }
-      if todo.nil?
-        schedule = IceCube::Schedule.from_yaml(ts.schedule)
-        todos << self.my_todos.build(todo_schedule_id: ts.id, due_date: date ) if schedule.occurs_on?(date)
+  def todos(start_date = Date.today, end_date = Date.today)
+    todos = []
+    (start_date .. end_date).each do |date|
+      local_todos = self.my_todos.where('due_date = ?', date).map.to_a
+      self.todo_schedules.where('start_date <= ? AND (end_date IS NULL OR end_date <= ?)', date, date).find_each do |ts|
+        todo = local_todos.find{ |td| td.todo_schedule_id == ts.id }
+        if todo.nil?
+          schedule = IceCube::Schedule.from_yaml(ts.schedule)
+          local_todos << self.my_todos.build(todo_schedule_id: ts.id, due_date: date ) if schedule.occurs_on?(date)
+        end
       end
+      todos.concat(local_todos)
     end
     todos
   end
