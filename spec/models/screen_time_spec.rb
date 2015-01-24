d590eae064b06c03d51edfba2d1fdd13b6e8df5d
Michael Kazmier <kaz@kaztopia.com>
2015-01-24 08:03:12 -0700

    working model tests, updates to authorizations

--- spec/models/screen_time_spec.rb
+++ spec/models/screen_time_spec.rb
@@ -6,12 +6,4 @@ RSpec.describe ScreenTime, :type => :model do
     expect(screentime.valid?).to be_truthy
   end
 
-  it 'should format time as a countdown' do
-    screentime = FactoryGirl.create( :screen_time, maxtime: (60*60+60*33+46) )
-    expect(screentime.to_s).to eq('1:33:46')
-
-    # and have leading zeros
-    screentime.maxtime =     (60*60+60*3+6)
-    expect(screentime.to_s).to eq('1:03:06')
-  end
 end
