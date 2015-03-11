ff4064db63c44c9ffb33498e8c28814ed0f56eac
Michael Kazmier <kaz@kaztopia.com>
2015-03-11 17:24:47 -0600

    lots of refactoring for CRM functions

---
+++ app/controllers/admin/activity_templates_controller.rb
@@ -0,0 +1,45 @@
+class Admin::ActivityTemplatesController < AdminController
+  load_and_authorize_resource
+
+  respond_to :html
+
+  def index
+    @activity_templates = ActivityTemplate.all
+    respond_with(@activity_templates)
+  end
+
+  def show
+    respond_with(@activity_template)
+  end
+
+  def new
+    @activity_template = ActivityTemplate.new
+    respond_with(@activity_template)
+  end
+
+  def edit
+  end
+
+  def create
+    @activity_template = ActivityTemplate.new(activity_template_params)
+    @activity_template.save
+    respond_with(@activity_template)
+  end
+
+  def update
+    @activity_template.update(activity_template_params)
+    respond_with(@activity_template)
+  end
+
+  def destroy
+    @activity_template.destroy
+    respond_with(@activity_template)
+  end
+
+  private
+
+
+    def activity_template_params
+      params.require(:activity_template).permit(:name, :description, :rec_min_age, :rec_max_age, :cost, :reward, :time_block, :restricted, :activity_type_id)
+    end
+end
