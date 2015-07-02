7036ff8f2523d2f20fe2223cb010fb404b8800bc
Michael Kazmier <kaz@kaztopia.com>
2015-07-02 10:41:11 -0600

    refactor todos, remove todo_groups, add recommended and default age ranges to todo_templates

--- spec/factories/todo_templates.rb
+++ spec/factories/todo_templates.rb
@@ -5,7 +5,10 @@ FactoryGirl.define do
     name { Faker::Lorem.sentence }
     description { Faker::Lorem.sentence }
     kudos 0
-    active true
+    rec_min_age { rand(2..8) }
+    rec_max_age { rec_min_age + rand(2..8) }
+    def_min_age { rec_min_age }
+    def_max_age { rec_max_age - 1  }
 
     after(:build) { |tt| tt.rule = IceCube::Rule.daily.to_yaml }
   end
