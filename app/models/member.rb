3abc083b39433dbc6375b56466faa3ab5487747c
Michael Kazmier <kaz@kaztopia.com>
2015-07-06 16:28:55 -0600

    basic working avatar for members

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,6 +12,9 @@ class Member < ActiveRecord::Base
   has_one :screen_time_schedule
   has_many :api_keys
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   # ensure we have a secure password even if the user has no password
   before_save :secure_password
 
