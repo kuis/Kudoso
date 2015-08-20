f27545a4f497937f15e41b806c143ff9e0101ac2
Michael Kazmier <kaz@kaztopia.com>
2015-08-20 08:46:42 -0600

    migrated to cancancan; updated testing; add themes to admin section

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,4 +1,4 @@
-class ThemesController < ApplicationController
+class Admin::ThemesController < AdminController
   before_action :set_theme, only: [:show, :edit, :update, :destroy]
 
   # GET /themes
@@ -28,7 +28,7 @@ class ThemesController < ApplicationController
 
     respond_to do |format|
       if @theme.save
-        format.html { redirect_to @theme, notice: 'Theme was successfully created.' }
+        format.html { redirect_to admin_themes_url, notice: 'Theme was successfully created.' }
         format.json { render :show, status: :created, location: @theme }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ThemesController < ApplicationController
   def update
     respond_to do |format|
       if @theme.update(theme_params)
-        format.html { redirect_to @theme, notice: 'Theme was successfully updated.' }
+        format.html { redirect_to admin_themes_url, notice: 'Theme was successfully updated.' }
         format.json { render :show, status: :ok, location: @theme }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ThemesController < ApplicationController
   def destroy
     @theme.destroy
     respond_to do |format|
-      format.html { redirect_to themes_url, notice: 'Theme was successfully destroyed.' }
+      format.html { redirect_to admin_themes_url, notice: 'Theme was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
