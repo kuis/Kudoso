01da32002a988a021f17b60c5689e14f7559a031
Mike Kazmier <kaz@kaztopia.com>
2015-11-23 13:33:09 -0700

    new schedule recurring rules API work

--- spec/models/todo_schedule_spec.rb
+++ spec/models/todo_schedule_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe TodoSchedule, :type => :model do
     expect(todo_s.valid?).to be_falsey
     expect(todo_s.errors[:start_date].any?).to be_truthy
   end
+
+  it 'should allow daily recurring rules to be set' do
+    todo_s = FactoryGirl.create(:todo_schedule, end_date: Date.today)
+    todo_s.schedule_rrules.each {|rr| rr.destroy}
+    todo_s.reload
+    expect(todo_s.schedule.occurs_on?(3.days.from_now)).to be_falsey
+    todo_s.update_attribute(:end_date, nil)
+    rrule = FactoryGirl.create(:schedule_rrule, todo_schedule_id: todo_s.id)
+    todo_s.reload
+    expect(todo_s.schedule.occurs_on?(3.days.from_now)).to be_truthy
+  end
+
 end
